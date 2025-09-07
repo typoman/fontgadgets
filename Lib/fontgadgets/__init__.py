60a9637034ab7e0e3d5b30cce42e48a659ce869e
Bahman Eslami <eslami.bahman@gmail.com>
2025-09-07 16:18:08 +0200

    Relocate merged modules to Lib/fontgadgets/

--- Lib/fontgadgets/__init__.py
+++ Lib/fontgadgets/__init__.py
@@ -1,5 +1,26 @@
-import fontGadgets.objects.font
-import fontGadgets.objects.glyph
-import fontGadgets.objects.kerning
-import fontGadgets.objects.features
-import fontGadgets.objects.groups
+from fontgadgets.tools import *
+
+def decorateAll():
+    """
+    Decorates all the fontParts and defcon object from this package extensions.
+    """
+    from fontgadgets import extensions as _fontgadgets_extensions
+
+    _fontgadgets_extensions.loadExtensions()
+
+
+def reloadAll():
+    """
+    This functions is only used inside RF and can be used to relaod all the
+    fontgadgets modules. This is used during the development of this package
+    inside RF.
+    """
+    assert getEnvironment() == "RoboFont"
+    import fontgadgets.tools
+    from fontgadgets.extensions.robofont.tools import reloadSubModules
+
+    reloadSubModules("fontgadgets.tools")
+    fontgadgets.tools.DEBUG = True
+    reloadSubModules("fontgadgets", skipSubModules=set(["tools"]))
+    fontgadgets.tools.DEBUG = False
+    reloadSubModules("fontgadgets.robofont")
